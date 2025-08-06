348bde7547da20b833ce1950da4059f1918037c8
Carl Fampo <cfampo@iu.edu>
2025-08-06 15:22:47 -0400

    Changes to the gameboard

--- script.js
+++ script.js
@@ -93,3 +93,14 @@ const game = {
     }
 };
 
+const gameboard = document.querySelector(".gameboard");
+const startForm = document.getElementById("start-form");
+startForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const player1 = document.getElementById("player1").value.trim();
+    const player2 = document.getElementById("player2").value.trim();
+    game.start(player1, player2);
+    startForm.style.display = "none";
+    gameboard.style.display = "grid";
+})
+
